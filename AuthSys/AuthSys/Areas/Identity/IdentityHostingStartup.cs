c8dcf84ac3c446d3709e3fa9ee260ced5fb261c5
991591516 <patraxit@sheridancollege.ca>
2022-03-16 13:34:33 -0400

    Commiting Code

---
+++ AuthSys/AuthSys/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AuthSys.Areas.Identity.Data;
+using AuthSys.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthSys.Areas.Identity.IdentityHostingStartup))]
+namespace AuthSys.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDBContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+                    .AddEntityFrameworkStores<AuthDBContext>();
+            });
+        }
+    }
+}
