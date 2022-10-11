7a06c5617996e40ce259e4abb5e52cbe90b0538a
991591516 <patraxit@sheridancollege.ca>
2022-10-11 10:41:21 -0400

    Adding PDF Viewer

---
+++ AuthSys/AuthSys/Controllers/Files1Controller.cs
@@ -0,0 +1,66 @@
+﻿using AuthSys.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthSys.Controllers
+{
+    public class Files1Controller : Controller
+    {
+        IHostingEnvironment _hostingEnvironment = null;
+        
+        public Files1Controller(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        
+        
+        [HttpGet]
+        public IActionResult Index(string fileName="")
+        {
+            FileClass fileObj = new FileClass();
+            fileObj.Name = fileName;
+
+            string path = $"{_hostingEnvironment.WebRootPath}\\files\\";
+            int nId = 1;
+
+            foreach(string pdfPath in Directory.EnumerateFiles(path, "*.pdf"))
+            {
+                fileObj.Files.Add(new FileClass()
+                {
+                    FileId=nId++,
+                    Name = Path.GetFileName(pdfPath),
+                    Path = pdfPath
+                });
+            }
+
+
+
+            return View(fileObj);
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormFile file, [FromServices] IHostingEnvironment hostingEnvironment)
+        {
+            string fileName = $"{hostingEnvironment.WebRootPath}\\files\\{file.FileName}";
+            using(FileStream fileStream = System.IO.File.Create(fileName))
+            {
+                file.CopyTo(fileStream);
+                fileStream.Flush();
+
+            }
+            return Index();
+        }
+
+        public IActionResult PDFViewerNewTab(string fileName)
+        {
+            string path = _hostingEnvironment.WebRootPath + "\\files\\" + fileName;
+            return File(System.IO.File.ReadAllBytes(path), "application/pdf");
+        }
+    }
+}
---
+++ AuthSys/AuthSys/Models/FileClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthSys.Models
+{
+    public class FileClass
+    {
+        public int FileId { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public string Path { get; set; } = "";
+
+        public List<FileClass> Files { get; set; } = new List<FileClass>();
+
+    }
+}
